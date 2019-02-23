bab73d552f62a82dc94b4e45e0c7f5d8ee4b9a3d
1875167603 <1875167603@qq.com>
2019-02-23 10:32:59 +0800

    test

---
+++ test/runMaven.bat
@@ -0,0 +1,2 @@
+cd C:\Users\xqy\Desktop\github\dty717\teamviewer\test
+mvn tomcat7:run
---
+++ test/src/main/java/test/web/HelloController.java
@@ -0,0 +1,18 @@
+package test.web; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+ 
+    @RequestMapping(value = { "/hello" ,"/Hello"})
+    @ResponseBody
+    public String example() {
+        
+        return "Hello World";
+    }
+}
