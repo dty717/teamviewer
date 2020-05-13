e89e8e229139c7b6156ea1c1ecdbab7a31c999b9
1875167603 <1875167603@qq.com>
2020-05-13 17:36:09 +0800

    cors

--- fileSystem/src/main/java/fileSystem/web/UserController.java
+++ fileSystem/src/main/java/fileSystem/web/UserController.java
@@ -13,6 +13,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import flexjson.JSONSerializer;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -23,6 +30,36 @@ public class UserController {
     @RequestMapping(value = { "/login"})
     public String login() {
         return "/views/login.html";
+    }
+    
+    public HttpHeaders getJsonHeaders() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "application/json");
+        return headers;
+    }
+    
+    @RequestMapping(value="/rest/security/login-page", method = RequestMethod.GET)
+    public ResponseEntity<String> apiLoginPage() {
+        return new ResponseEntity<String>(getJsonHeaders(), HttpStatus.UNAUTHORIZED);
+    }
+    
+
+
+    
+
+    @RequestMapping(value="/rest/security/authentication-failure", method = RequestMethod.GET)
+    public ResponseEntity<String> apiAuthenticationFailure() {
+        // return HttpStatus.OK to let your front-end know the request completed (no 401, it will cause you to go back to login again, loops, not good)
+        // include some message code to indicate unsuccessful login
+        return new ResponseEntity<String>("{\"success\" : false, \"message\" : \"authentication-failure\"}", getJsonHeaders(), HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/rest/security/default-target", method = RequestMethod.GET)
+    public ResponseEntity<String> apiDefaultTarget() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        // exclude/include whatever fields you need
+        String userJson = new JSONSerializer().exclude("*.class", "*.password").serialize(authentication);
+        return new ResponseEntity<String>(userJson, getJsonHeaders(), HttpStatus.OK);
     }
 
 }
