137626f8c08e9625f79059f4060e8d436a472c40
1875167603 <1875167603@qq.com>
2020-07-19 09:17:13 +0800

    by dty717

--- fileSystem/src/main/java/fileSystem/web/HelloController.java
+++ fileSystem/src/main/java/fileSystem/web/HelloController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import java.io.*;
 import java.util.LinkedHashMap;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class HelloController {
  
